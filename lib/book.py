d0e71c26d65e9200f26dea9cc714b30174f3a0d4
stanleywanjau <stanleywanjau371@gmail.com>
2023-12-11 13:30:42 +0300

    completed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__ (self,title="And Then There Were None",page_count=272):
+        self._page_count=None
+        self._title=None
+        self.title = title
+        self.page_count = page_count
+
+    def get_page_count(self):
+        return self._page_count
+    def set_page_count(self,count):
+        if type(count)==int:
+            self._page_count = count
+        else:
+            print("page_count must be an integer")
+    page_count = property(get_page_count,set_page_count)
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     pass
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self,brand,size ):
+        self._brand = None
+        self._size = None
+        self.size = size
+        self.brand=brand
+        self.condition = "used"
+    def get_size(self):
+        return self._size
+    def set_size(self,count):
+        if type(count)== int:
+            self._size = count
+        else:
+            print("size must be an integer")
+    size = property(get_size, set_size)
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
     pass
+    
